3a40b26e18195c4c197679036858c4b6f9690880
syu <syukunt@icloud.com>
2021-04-22 19:27:24 +0900

    抽出ジャンルの選択GUI実装

--- gui-test.py
+++ gui-test.py
@@ -1,17 +1,4 @@
 import PySimpleGUI as gui
 from PySimpleGUI.PySimpleGUI import Window 
 
-layout = [
-    [gui.Text("BIG UNNKO")],
-    [gui.Text("World")],
-    [gui.InputText(key="-IN-")],
-    [gui.Submit(), gui.Cancel()]
-]
-
-window = gui.Window-("test-gui", layout=layout, size=(400, 400))
-
-event, val = window.read()
-window.close()
-
-text_in = val["-IN-"]
-gui.popup("OKi", text_in)
+gui.theme_previewer()
--- gui.py
+++ gui.py
@@ -5,12 +5,13 @@ layout = [
     [sig.Text("エリア入力"),],
     [sig.Input(key='-IN1-')],
     [sig.Text("抽出ジャンル選択")],
-    [sig.Text("準備中")],
+    [sig.OptionMenu(["ヘアサロン", "ネイル・まつげサロン", "リラクサロン", "エステサロン", "美容クリニック","すべてのジャンル"])],
     [sig.Text("保存先を選択"), sig.InputText(), sig.FolderBrowse(key="save_folder")],
-    [sig.Button("抽出開始")]
+    [sig.Button("抽出実行")]
 
 ]
 
+sig.theme('SystemDefault')
 width = 700
 height = 300
 win = sig.Window(title="HotPepperBeautyスクレイピングツール", layout=layout, size=(width, height))
