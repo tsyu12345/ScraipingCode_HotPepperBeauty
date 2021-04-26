51e75d6ebca97e0c9f4027cd529f791f50bd84c8
syu <syukunt@icloud.com>
2021-04-26 20:18:50 +0900

    並列処理の試験ー失敗ー一からやりなおす。

--- scrap.py
+++ scrap.py
@@ -1,5 +1,4 @@
 from openpyxl.worksheet.dimensions import SheetDimension
-from requests.api import options
 from selenium import webdriver
 from selenium.common.exceptions import InvalidArgumentException, InvalidSwitchToTargetException, NoSuchElementException, TimeoutException, WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -10,10 +9,12 @@ import time
 import datetime
 import re
 import requests as rq
-import concurrent.futures as cf
+import threading
+from concurrent.futures import (ThreadPoolExecutor, wait)
 
 
 class Job:
+    lock = threading.Lock()
 
     def __init__(self, driver_path, books_path, area_name, store_class):
         self.book_path = books_path
@@ -67,7 +68,7 @@ class Job:
             self.sheet.cell(row=i+2, column=8, value=link_list[i])
         self.book.save(self.book_path)
 
-    def info_scrap(self, url, area_name, index):
+    def info_scrap(self, url, index):
         dt_now = datetime.datetime.now()
         year = str(dt_now.year)
         month = str(dt_now.month)
@@ -96,16 +97,17 @@ class Job:
             tel_num_tag = soup_tel.select_one('table > tr > td')
             tel_num = tel_num_tag.get_text()
             print("TEL : " + tel_num)
-            self.sheet.cell(row=index, column=4, value=tel_num)
         except:
             pass
 
         # ヘッダー画像の有無
         try:
             self.driver.find_element_by_css_selector('#jsiNavCarousel > div')
-            self.sheet.cell(row=index, column=14, value="有")
+            head_img_yn = "有"
+            #self.sheet.cell(row=index, column=14, value="有")
         except NoSuchElementException:
-            self.sheet.cell(row=index, column=14, value="無")
+            head_img_yn = "無"
+            #self.sheet.cell(row=index, column=14, value="無")
             pass
 
         table_value = soup.select('div.mT30 > table > tbody > tr > td')
@@ -121,35 +123,13 @@ class Job:
                     '東京都|北海道|(?:京都|大阪)府|.{2,3}県', all_address)  # 県名とそれ以降を分離
                 prefecture = prefecture_search.group()  # 県名
                 if prefecture != self.area_name:
+                    self.sheet.cell(row=index, column=8, value="")
                     return False
                 jis_code = self.call_jis_code(prefecture)
                 municipality = address_low[1]  # それ以降
         print("都道府県：" + prefecture)
         print("市区町村番地：" + municipality)
 
-        # 他の情報処理
-        for i in range(2, len(table_value)):
-            for c in range(1, self.sheet.max_column):
-                if table_menu[i].get_text() == self.sheet.cell(row=1, column=c).value:
-                    self.sheet.cell(row=index, column=c,
-                                    value=table_value[i].get_text())
-                    break
-
-        """
-        anounce_access = table[2].get_text()
-        bs_time = table[3].get_text()
-        holiday = table[4].get_text()
-        credit = table[5].get_text()
-        fee = table[6].get_text()
-        seat_cnt = table[7].get_text()
-        staff_cnt = table[8].get_text()
-        parking = table[9].get_text()
-        commitment = table[10].get_text()
-        try:
-            remarks = table[11].get_text()
-        except IndexError:
-            remarks = ""
-"""
         catch_copy_tag = soup.select_one(
             '#mainContents > div.pH10.mT25 > div:nth-child(1) > p > b > strong')
         catch_copy = catch_copy_tag.get_text()
@@ -164,9 +144,10 @@ class Job:
             '#mainContents > div.pH10.mT25 > div:nth-child(1) > div > div.slnTopImg.jscThumbCarousel > div.slnTopImgCarouselWrap.jscThumbWrap > ul > li')
         slide_cnt = len(slide_img_tag)
 
+# write Excel
         self.sheet.cell(row=index, column=2, value=store_name)
         self.sheet.cell(row=index, column=3, value=st_name_kana)
-        #self.sheet.cell(row=index, column=4, value=tel_num)
+        self.sheet.cell(row=index, column=4, value=tel_num)
         self.sheet.cell(row=index, column=5, value=jis_code)
         self.sheet.cell(row=index, column=6, value=prefecture)
         self.sheet.cell(row=index, column=7, value=municipality)
@@ -174,18 +155,14 @@ class Job:
         self.sheet.cell(row=index, column=13, value=pankuzu)
         self.sheet.cell(row=index, column=16, value=slide_cnt)
         self.sheet.cell(row=index, column=17, value=catch_copy)
-        """
-        self.sheet.cell(row=index, column=18, value=anounce_access)
-        self.sheet.cell(row=index, column=19, value=bs_time)
-        self.sheet.cell(row=index, column=20, value=holiday)
-        self.sheet.cell(row=index, column=21, value=credit)
-        self.sheet.cell(row=index, column=23, value=fee)
-        self.sheet.cell(row=index, column=24, value=seat_cnt)
-        self.sheet.cell(row=index, column=25, value=staff_cnt)
-        self.sheet.cell(row=index, column=26, value=parking)
-        self.sheet.cell(row=index, column=27, value=commitment)
-        self.sheet.cell(row=index, column=28, value=remarks)
-        """
+        self.sheet.cell(row=index, column=14, value=head_img_yn)
+        # 他の情報処理
+        for i in range(2, len(table_value)):
+            for c in range(1, self.sheet.max_column):
+                if table_menu[i].get_text() == self.sheet.cell(row=1, column=c).value:
+                    self.sheet.cell(row=index, column=c,
+                                    value=table_value[i].get_text())
+                    break
         self.book.save(self.book_path)
 
     def call_jis_code(self, key):
@@ -248,37 +225,55 @@ class Job:
                 self.sheet.cell(row=index, column=c, value=" ")
         self.book.save(self.book_path)
 
-    #指定行までのURLリストを生成し返す
+    # 指定行までのURLリストを生成し返す
     def make_url_list(self, stop_index):
         url_list = []
         for i in range(2, stop_index):
             url_list.append(self.sheet.cell(row=i, column=8).value)
         return url_list
 
-"""
-    def check_prefecture(self, area_name, index):
-        for i in range(2, self.sheet.max_row+1):
-            if self.sheet.cell(row=index, column=6).value != area_name:
-                for c in range(1, self.sheet.max_column+1):
-                    self.sheet.cell(row=index, column=c, value=None)
-        self.book.save(self.book_path)
-"""
 
 if __name__ == "__main__":
     st_time = time.time()
     job = Job("chromedriver_win32\chromedriver.exe",
-              "【サンプル】ホットペッパービューティー copy - コピー.xlsx", "北海道", "ヘアサロン")
-    job.url_scrap()
-    """
-    # 取得したURLを４つの配列へ分割格納
-    url_set1 = []
-    url_set2 = []
-    url_set3 = []
-    url_set4 = []
-    append_cnt = job.sheet.max_row / 4
+              "【サンプル】ホットペッパービューティー copy.xlsx", "北海道", "ヘアサロン")
+
+    def process(s_inedx, end_index):
+        for i in range(s_inedx, end_index+1):
+            if job.sheet.cell(row=i, column=8).value == None:
+                job.url_scrap()
+        for i in range(s_inedx, end_index+1):
+            try:
+                if job.sheet.cell(row=i, column=6).value == None:
+                    print(job.sheet.cell(row=i, column=8).value)
+                    job.info_scrap(job.sheet.cell(row=i, column=8).value, i)
+                    job.apper_adjust(index=i)
+            except WebDriverException:
+                print("WebDriver Exception occured. Retrying...")
+                job.driver.close()
+                job.info_scrap(job.sheet.cell(row=i, column=8).value, i)
+                job.apper_adjust(index=i)
+    
+    th1 = threading.Thread(target=process(2, 502))
+    th2 = threading.Thread(target=process(503, 1003))
+    th3 = threading.Thread(target=process(1004, 1504))
+    th4 = threading.Thread(target=process(1505, 2005))
+    th5 = threading.Thread(target=process(2006, 2506))
+    th1.start()
+    print("thred1...")
+    th2.start()
+    print("thred2...")
+    th3.start()
+    print("thred3...")
+    th4.start()
+    print("thred4...")
+    th5.start()
+    print("thred5...")
+
+""" 
     for i in range(2, job.sheet.max_row+1):
-        if i % append_cnt == 0:
-"""
+        if job.sheet.cell(row=i, column=8).value == None:
+            job.url_scrap()
     for i in range(2, job.sheet.max_row+1):
         try:
             if job.sheet.cell(row=i, column=6).value == None:
@@ -290,6 +285,7 @@ if __name__ == "__main__":
             job.driver.close()
             job.info_scrap(job.sheet.cell(row=i, column=8).value, i)
             job.apper_adjust(index=i)
+"""
 """
     print("==WARNING==")
     print("checking scrap data.!!DO NOT STOP THIS PROGRAM!!.")
